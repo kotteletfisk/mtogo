5514064bf194cb29dc931a506d86174baa825378
kotteletfisk <ostekugle@gmail.com>
2025-11-09 19:10:35 +0100

    yes

--- services/example-service/src/main/java/com/mtogo/example/App.java
+++ services/example-service/src/main/java/com/mtogo/example/App.java
@@ -1,7 +1,7 @@
 package com.mtogo.example;
 
 /**
- * Hello world! again yes! and again! and again and again.. and again and yes
+ * Hello world! again yes! and again! and again and again.. and again and yes yy
  */
 public class App {
     public static void main(String[] args) {
