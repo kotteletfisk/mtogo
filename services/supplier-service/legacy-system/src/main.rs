32e938c0dc033b9b903623cb554b873635827a6e
kotteletfisk <ostekugle@gmail.com>
2025-11-19 19:03:46 +0100

    create legacy backend

--- services/supplier-service/legacy-system/src/main.rs
+++ services/supplier-service/legacy-system/src/main.rs
@@ -1,12 +1,12 @@
-use axum::http::{StatusCode};
+use axum::Router;
+use axum::http::StatusCode;
 use axum::routing::post;
-use axum::{Router};
-use tower_http::services::ServeDir;
-use serde::{Deserialize, Serialize};
 use quick_xml::{de, se};
+use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::io::Write;
 use std::net::TcpStream;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 
@@ -14,30 +14,25 @@ async fn main() {
     let app = app();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("server on {}", listener.local_addr().unwrap());
-    axum::serve(listener,app).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
 
-fn app () -> Router {
+fn app() -> Router {
     let static_files = ServeDir::new("./assets");
-    
+
     Router::new()
         .fallback_service(static_files)
         .route("/submit", post(handle_submit))
 }
 
-async fn handle_submit(body: String) -> Result<(StatusCode, String), (StatusCode, String)>{
-    
+async fn handle_submit(body: String) -> Result<(StatusCode, String), (StatusCode, String)> {
     match de::from_str::<Order>(&body) {
         Ok(order) => {
             println!("Received Order: {:?}", order);
 
             match write_to_db(order) {
-                Ok(()) => {
-                    Ok((StatusCode::CREATED, String::from("Order Created")))
-                }
-                Err(e) => {
-                    Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
-                }
+                Ok(()) => Ok((StatusCode::CREATED, String::from("Order Created"))),
+                Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
             }
         }
         Err(e) => {
@@ -48,13 +43,12 @@ async fn handle_submit(body: String) -> Result<(StatusCode, String), (StatusCode
 }
 
 fn write_to_db(order: Order) -> Result<(), Box<dyn Error>> {
-
     let xml_payload = se::to_string(&order)?;
     let mut stream = TcpStream::connect("127.0.0.1:1984")?;
 
     stream.write_all(xml_payload.as_bytes())?;
     stream.shutdown(std::net::Shutdown::Write)?;
-    
+
     Ok(())
 }
 
@@ -68,7 +62,7 @@ struct Order {
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "PascalCase")]
 struct OrderLine {
-    item_id : String,
+    item_id: String,
     amount: i32,
     unit_price: f32,
     sub_total: f32,
