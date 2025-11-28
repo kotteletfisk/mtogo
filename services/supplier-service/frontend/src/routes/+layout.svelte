71424b110bea8dcc381014be8b49279dd05489ba
kotteletfisk <ostekugle@gmail.com>
2025-11-28 14:20:35 +0100

    frontend order components

--- services/supplier-service/frontend/src/routes/+layout.svelte
+++ services/supplier-service/frontend/src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import "$lib/main.css";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Supplier</title>
 </svelte:head>
+
+<div class="header">
+	<h3>Supplier header</h3>
+</div>
 
 {@render children()}
---
+++ services/supplier-service/frontend/src/routes/orderlist.svelte
@@ -0,0 +1,35 @@
+<script>
+    let { orders, setActiveOrder } = $props();
+
+</script>
+
+<div class="container">
+  <h1>Placed Orders</h1>
+
+  <div class="order-list">
+    <table>
+      <thead>
+        <tr>
+          <th>Order ID</th>
+          <th>Customer ID</th>
+          <th>Created</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each orders as o}
+          <tr onclick={() => setActiveOrder(o)}>
+            <td>
+              {o.orderId}
+            </td>
+            <td>
+              {o.customerId}
+            </td>
+            <td>
+              {o.orderCreated}
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+</div>
