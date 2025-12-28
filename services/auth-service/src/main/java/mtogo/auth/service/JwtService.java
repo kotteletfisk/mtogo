cad8e0f8ef6ea869689c4e981f13048720841b9e
MrJustMeDahl <nico.dahl93@gmail.com>
2025-12-28 13:07:36 +0100

    Javadoc
    Added javadoc documentation for auth-service

--- services/auth-service/src/main/java/mtogo/auth/service/JwtService.java
+++ services/auth-service/src/main/java/mtogo/auth/service/JwtService.java
@@ -13,17 +13,32 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * @author MrJustMeDahl
+ * Class tasked with creating Json web tokens.
+ */
 public class JwtService {
 
     private static final Logger log = LoggerFactory.getLogger(JwtService.class);
     private final Algorithm algorithm;
     private final String issuer = "mtogo-auth";
 
+    /**
+     * @author MrJustMeDahl
+     * @param privateKeyPem - the private key being used to sign tokens with in a String pem format.
+     * Constructor - Being used to initialize the service with the private key it needs for signing the tokens.
+     */
     public JwtService(String privateKeyPem) {
         RSAPrivateKey privateKey = parsePrivateKey(privateKeyPem);
         this.algorithm = Algorithm.RSA256(null, privateKey);
     }
 
+    /**
+     * @author MrJustMeDahl
+     * @param pem - the private key as a String in pem format.
+     * @return An RSAPrivateKey.
+     * Method - used to parse the private key from pem format to the class RSAPrivateKey.
+     */
     private RSAPrivateKey parsePrivateKey(String pem) {
         try {
             String base64 = pem
@@ -40,6 +55,14 @@ public class JwtService {
         }
     }
 
+    /**
+     * @author MrJustMeDahl
+     * @param email - Auth identifier, being added to the token as the subject.
+     * @param roles - List of which system roles the auth has.
+     * @param actorId - The entity ID of the auth, an internal reference to their identity.
+     * @return The JWT as a String.
+     * Method - used for creating and signing the JWT token with a payload, identifying an auth and their roles.
+     */
     public String createToken(String email, List<String> roles,  String actorId) {
         Instant now = Instant.now();
         Instant exp = now.plusSeconds(3600);
